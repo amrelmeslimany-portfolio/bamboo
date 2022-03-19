bff9354d736ae13377c85fcd3fa0eec4cd223676
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2022-03-19 23:23:07 +0200

    Create profiles page

--- js/app.js
+++ js/app.js
@@ -15,6 +15,13 @@ $(function () {
   const paymentMethods = $(".payment-methods .list-group-item");
   const nextAfterSelectPayment = $(".toggle-next-btn");
 
+  // Profile pages vars
+  const profileInfoWrap = $(".profile-info-wrap");
+  const myorderWrap = $(".myorder-wrap");
+  const seemoreWrap = $(".seemore-wrap");
+  const phoneKey = $("#phonekey");
+  const editImgProfile = $(".image-profile");
+
   // Carousel Settings Vars
   const PUBLIC_SETTING_CAROUSEL = (margin = 30, other) => ({
     autoWidth: true,
@@ -32,6 +39,95 @@ $(function () {
       loop: false,
     },
   };
+
+  /*
+    * Address page vars
+    ** This is var for inputs tags of address type select
+
+    <!-- If Selected address type == house -->
+                        <div class="mb-3 toggled-inputs">
+                            <label for="housenumber" class="form-label">House Number</label>
+                            <input type="text" name="housenumber" id="housenumber"
+                                class="form-control rounded-4" placeholder="EX: 744524">
+                        </div>
+
+                        <!-- If Selected address type == aparment  && == office -->
+                        <div class="mb-3 toggled-inputs">
+                            <label for="building" class="form-label">Building</label>
+                            <input type="text" name="building" id="building" class="form-control rounded-4"
+                                placeholder="EX: building">
+                        </div>
+                        <div class="mb-3 toggled-inputs">
+                            <label for="floor" class="form-label">Floor</label>
+                            <input type="text" name="floor" id="floor" class="form-control rounded-4"
+                                placeholder="EX: floor">
+                        </div>
+
+
+                        <!-- If Selected address type == aparment -->
+                        <div class="mb-3 toggled-inputs">
+                            <label for="apartmentnumber" class="form-label">Apartment Number</label>
+                            <input type="text" name="apartmentnumber" id="apartmentnumber"
+                                class="form-control rounded-4" placeholder="EX: apartmentnumber">
+                        </div>
+
+                        <!-- If Selected address type == office -->
+                        <div class="mb-3 toggled-inputs">
+                            <label for="officenumber" class="form-label">Office Number</label>
+                            <input type="text" name="officenumber" id="officenumber"
+                                class="form-control rounded-4" placeholder="EX: officenumber">
+                        </div>
+  */
+
+  const ADDRESS_INPUTS_TEMP = [
+    {
+      type: ["house"],
+      htmlTemp: `
+                <div class="mb-3 toggled-inputs">
+                    <label for="housenumber" class="form-label">House Number</label>
+                    <input type="text" name="housenumber" id="housenumber"
+                        class="form-control rounded-4" placeholder="EX: 744524">
+                </div>
+              `,
+    },
+    {
+      type: ["apartment"],
+      htmlTemp: `
+                <div class="mb-3 toggled-inputs">
+                    <label for="apartmentnumber" class="form-label">Apartment Number</label>
+                    <input type="text" name="apartmentnumber" id="apartmentnumber"
+                        class="form-control rounded-4" placeholder="EX: apartmentnumber">
+                </div>
+              `,
+    },
+    {
+      type: ["office"],
+      htmlTemp: `
+        <div class="mb-3 toggled-inputs">
+        <label for="officenumber" class="form-label">Office Number</label>
+        <input type="text" name="officenumber" id="officenumber"
+            class="form-control rounded-4" placeholder="EX: officenumber">
+    </div>
+              `,
+    },
+    {
+      type: ["office", "apartment"],
+      htmlTemp: `
+        <div class="mb-3 toggled-inputs">
+        <label for="building" class="form-label">Building</label>
+        <input type="text" name="building" id="building" class="form-control rounded-4"
+            placeholder="EX: building">
+    </div>
+    <div class="mb-3 toggled-inputs">
+        <label for="floor" class="form-label">Floor</label>
+        <input type="text" name="floor" id="floor" class="form-control rounded-4"
+            placeholder="EX: floor">
+    </div>
+              `,
+    },
+  ];
+  const addressTypeWrap = $(".address-type-wrap");
+  const toggledAddressInputs = $(".toggled-inputs");
 
   // Handle Loading page
   loading.hide(100, function () {
@@ -65,6 +161,22 @@ $(function () {
       ...PUBLIC_SETTING_CAROUSEL(30, {
         responsive: RESPONSIVE_CAROUSEL,
       }),
+    });
+  }
+  // favourite Products at profile page
+  if (
+    $(".products-wrap-carousel").length ||
+    $(".companies-wrap-carousel").length
+  ) {
+    // I used setTimeOut to wait elements loading
+    $("#favorites-tab").one("click", function () {
+      setTimeout(() => {
+        $(".products-wrap-carousel , .companies-wrap-carousel").owlCarousel({
+          ...PUBLIC_SETTING_CAROUSEL(30, {
+            responsive: RESPONSIVE_CAROUSEL,
+          }),
+        });
+      }, 300);
     });
   }
 
@@ -151,12 +263,35 @@ $(function () {
   }
 
   // Address
+  /*
+   * Change the select input by using selct2 plugin
+   * Handle the select address type
+   */
   if ($(".custom-select").length) {
     $(".wrap-input").each(function () {
-      $(".custom-select").select2({
-        dropdownParent: $(this),
-        width: "100%",
-      });
+      $(this)
+        .find(".custom-select")
+        .select2({
+          dropdownParent: $(this),
+          width: "100%",
+        });
+    });
+  }
+
+  if (addressTypeWrap.length) {
+    const addressTypeInput = addressTypeWrap.find("select");
+    // Set Defualt
+    loopONInputsAddressOBJ(addressTypeInput.val());
+
+    // Handle change of this input
+    addressTypeInput.change(function () {
+      let selectedValue = $(this).val();
+
+      // Clear the toggled input first
+      toggledAddressInputs.html("");
+
+      // IF Selected is HOUSE
+      loopONInputsAddressOBJ(selectedValue);
     });
   }
 
@@ -170,7 +305,76 @@ $(function () {
     });
   }
 
+  // Profile pages
+  /*
+   * Handle see more orders status
+   * Edit profile page
+   * Handle image profile preview
+   */
+
+  // Handle seemore sidebar
+  if (profileInfoWrap.length) {
+    const ordersList = myorderWrap.find(".orders-list");
+    const seemoreBTN = seemoreWrap.find("button");
+    let infowrapHeight = profileInfoWrap.height();
+
+    // At Tablets and Mobiles screen
+    if ($(window).innerWidth() <= 992) {
+      infowrapHeight = 550;
+    }
+
+    // Set height for "orders wrap section" (will make height == info height)
+    myorderWrap.height(
+      infowrapHeight - parseInt(myorderWrap.css("paddingTop")) + "px"
+    );
+
+    // if order list doesnt exists and its height isn't longer than the order section => hide see more wrap
+    if (ordersList.length == 0) {
+      seemoreWrap.remove();
+    } else {
+      if (ordersList.height() < myorderWrap.height()) {
+        seemoreWrap.remove();
+      }
+    }
+
+    // Handle onClick on button see more => remove the height
+    if (seemoreWrap.length)
+      seemoreBTN.click(function () {
+        myorderWrap.removeAttr("style");
+        seemoreWrap.remove();
+      });
+  }
+
+  // Handle phone key select with flag img
+  if (phoneKey.length) {
+    const defaultNameOfSelect = phoneKey.attr("name");
+    phoneKey.ddslick({
+      width: "100%",
+      defaultSelectedIndex: 0,
+      background: "white",
+      onSelected: function () {
+        // Set name to hidden input to post data
+        $(".dd-selected-value").attr("name", defaultNameOfSelect);
+      },
+    });
+  }
+
+  // Handle img profile
+  if (editImgProfile.find(".overflow-btn").length) {
+    const overflowBTN = editImgProfile.find(".overflow-btn");
+    const imgProfileInput = editImgProfile.find("#profileimg");
+
+    // On click on overflow -> click the hidden input upload type
+    overflowBTN.click(function () {
+      imgProfileInput.click();
+    });
+  }
+
   // Public functions
+  /*
+   ** Add "padding top" to body content becuse of navbar height
+   ** loop on every element in object of address inputs and select the sutible input for the sutible value
+   */
   function bodyPaddingTop() {
     let navbarHeight = mainHeader.find("nav.navbar").innerHeight();
     let navbarAlert = mainHeader.find(".alert");
@@ -181,5 +385,13 @@ $(function () {
     } else {
       return navbarHeight;
     }
+  }
+
+  function loopONInputsAddressOBJ(selectedType) {
+    ADDRESS_INPUTS_TEMP.forEach(function ({ type, htmlTemp }) {
+      if (type.includes(selectedType)) {
+        toggledAddressInputs.prepend(htmlTemp);
+      }
+    });
   }
 });
